76b20b5f7fa7ca51adf02c1eb42b1104303d5b52
illiteratewriter <manukoshyabraham@gmail.com>
2020-01-26 16:30:06 +0530

    feat: add web app to the project

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./index.web.js",
+    output: {
+        filename: "web/js/bundle.js",
+        sourceMapFilename: "web/js/bundle.map"
+    },
+    resolve: {
+       extensions: [ '.web.js', '.js' ],
+       symlinks: false
+     },
+    module: {
+        rules: [
+            {
+              test: /\.js$/,
+              include:[
+                path.resolve(__dirname, 'index.web.js'),
+                path.resolve(__dirname, 'src'),
+              ],
+              loader: ['babel-loader']
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+};
